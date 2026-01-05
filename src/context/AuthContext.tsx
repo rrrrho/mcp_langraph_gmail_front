182a4965aec198140c6bf50c995d788ef740cc23
rrrrho <rocioghillino@gmail.com>
2026-01-05 10:46:47 -0300

    minor log fix

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -77,7 +77,7 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
 export const useAuth = () => {
   const context = useContext(AuthContext);
   if (!context) {
-    throw new Error('useAuth debe ser usado dentro de un AuthProvider');
+    throw new Error('useAuth must be used inside an AuthProvider');
   }
   return context;
 };
